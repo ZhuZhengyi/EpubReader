2d159bf55e3fec52931290013a202b1d5e00060c
Zaiyiiiiiiiiii <wangzaiyiw@gmail.com>
2017-10-26 17:28:01 +0800

    暂时保存一下

--- src/bundle.js
+++ src/bundle.js
@@ -5640,7 +5640,7 @@ const updateBook = async (book) => {
     let doc = await DB.books.get(book._id)
     book._rev = doc._rev
     DB.books.put(book)
-    console.log("书籍信息已更新",doc)
+    console.log("书籍信息已更新")
 }
 /* harmony export (immutable) */ __webpack_exports__["b"] = updateBook;
 
@@ -44078,7 +44078,7 @@ exports = module.exports = __webpack_require__(12)(undefined);
 
 
 // module
-exports.push([module.i, "\n.reader-container {\n    width: 100%;\n    height: 100%;\n    display: flex;\n    align-items: center;\n    justify-content: center;\n    padding: 0 6em;\n    box-sizing: border-box;\n}\n@media screen and (max-width: 580px) {\n.reader-container {\n        padding: 0 2em;\n}\n}\n\n/* .buttonlist{\n                flex: 0;\n                bottom: 2em;\n                left: 2em;\n                -webkit-app-region: no-drag;\n            } */\n.reader {\n    max-width: 100%;\n    width: 1400px;\n    height: 80%;\n    flex-shrink: 0;\n    -webkit-app-region: no-drag;\n    font-family: \"default\";\n    user-select: none;\n    pointer-events: none;\n}\n.reader > * {\n    pointer-events: none;\n}\n", ""]);
+exports.push([module.i, "\n.reader-container {\n    width: 100%;\n    height: 100%;\n    display: flex;\n    align-items: center;\n    justify-content: center;\n    padding: 0 4em;\n    box-sizing: border-box;\n}\n@media screen and (max-width: 580px) {\n.reader-container {\n        padding: 0 2em;\n}\n}\n\n/* .buttonlist{\n                flex: 0;\n                bottom: 2em;\n                left: 2em;\n                -webkit-app-region: no-drag;\n            } */\n.reader {\n    max-width: 100%;\n    width: 1400px;\n    height: 80%;\n    flex-shrink: 0;\n    -webkit-app-region: no-drag;\n    font-family: \"default\";\n    user-select: none;\n    pointer-events: none;\n}\n.reader > * {\n    pointer-events: none;\n}\n", ""]);
 
 // exports
 
@@ -82554,165 +82554,156 @@ if (false) {(function () {
 //
 //
 
+
+
     
-    
-        
-    // import { do } from '@reactivex/rxjs/operator/do'
+// import { do } from '@reactivex/rxjs/operator/do'
 
-    /* harmony default export */ __webpack_exports__["a"] = ({
-        components: {
+/* harmony default export */ __webpack_exports__["a"] = ({
+    components: {
 
-        },
-        data() {
-            return {
-                book: null,
-                font: null
-            }
-        },
-        beforeDestroy(){
-            document.removeEventListener('keydown', this.keyEvent, false)
-            this.book = null
-            //  关闭监听事件。。。
-            this.$store._subscribers.pop()
-        },
-        async beforeMount(){
-            this.font = new FontFace("defaultText", "url(../static/fonts/SourceHanSerifSC-Regular.otf)", {})
-            await this.font.load()
-        },
-        async mounted() {
-            var _this = this
-            await this.loadBook()
-
-            //初始化渲染
-            let reader = this.$el.querySelector('.reader')
-            let rendition = this.book.renderTo(reader,{
-                width: "100%",
-                height: "100%",
-                manager: "continuous"
-            })           
-            
-            let font = this.font
-            rendition.hooks.content.register((content)=>{
-	            font.load().then(function (loadedFace) {
-		            content.document.fonts.add(loadedFace)
-	            })
-            })
-            
-            //初始化样式
-            console.log(this.book)
-            rendition.on("rendered",()=>{
-                console.log(rendition.themes)
-                rendition.themes.font("defaultText")
-                if(this.book){
-                    this.setStyle(this.$store.state.reader.book)
-                }
-            })
-
-            //开始渲染
-            rendition.display();
-            
-
-            //初始化按键事件，在对应方法里处理
-            //准备把按键改为主进程监听
-
-            this.$el.addEventListener('wheel', this.keyEvent, false)
-            this.$el.addEventListener('keydown', this.keyEvent, false)
-            this.book.on("renderer:keydown", _this.keyEvent.bind(_this))
-
-            //初始化store监听
-            this.$store.subscribe((mutation,state)=>{
-                var storeChange = new CustomEvent('storeChange',{detail: {mutation: mutation,state: state}})
-                this.$el.dispatchEvent(storeChange)
-            })
-
-            //。。。生成事件作为源
-            //加上消抖，手速太快的时候写入DB会有冲突，控制台一篇红色。。。
-
-            var storeHandler = __WEBPACK_IMPORTED_MODULE_1__reactivex_rxjs___default.a.Observable.fromEvent(this.$el, 'storeChange')
-            // .distinctUntilChanged((pre,cur)=> _.isEqual(pre.detail.state.reader.book, cur.detail.state.reader.book))
-            .throttleTime(100)
-            .subscribe((event)=>{
-                let state = event.detail.state
-                let mutation = event.detail.mutation
-                this.syncStore(mutation, state)
-            })            
-
-
-            // 初始化阅读位置自动保存
-            this.book.on('renderer:locationChanged',(location)=>{
-                this.$store.commit("SET_LASTREAD",{cfi:location})
-            })
-
-        },
-        methods: {
-            syncStore(mutation, state){
-                if(this.$route.path == "/reader"){   
-                    if(mutation.type == "SET_STYLE"){
-                        let book = state.reader.book
-                        this.setStyle(book)
-                        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__BookOps__["b" /* updateBook */])(book)
-                    }
-                    else if(mutation.type == "SET_LASTREAD"){
-                        let book = state.reader.book
-                        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__BookOps__["b" /* updateBook */])(book)
-                    }
-                }
-            },
-            setStyle(book){
-                console.log(book)
-                if(book.config && this.book){            
-                    if(book.config["font-size"]){
-                        this.book.rendition.themes.fontSize = book.config["font-size"]
-                        this.book.rendition.themes.default({
-                            "body": {
-                                'font-size': book.config["font-size"]+"px",
-                                color: 'purple'
-                            }
-                        });
-                        
-                    }
-                    if(book.config["line-height"]){                    
-                        this.book.rendition.themes.lineHeight = book.config["line-height"]
-                    }
-                }
-            },
-            keyEvent(event) {
-                if (event.key == "ArrowDown" || event.key == "ArrowRight" || event.deltaX > 0 || event.deltaY > 0) {
-                    console.log("下")
-                    this.pageNext()
-                }
-                else if (event.key == "ArrowUp" || event.key == "ArrowLeft" || event.deltaX < 0 || event.deltaY < 0) {
-                    console.log("上")
-                    this.pagePrev()
-                }
-            },
-            pageNext() {
-                this.book.rendition.next()
-
-            },
-            pagePrev() {
-                this.book.rendition.prev()
-            },
-            async loadBook() {
-                let bookData = this.$store.state.reader.book
-                if (bookData) {
-                    let cfi = bookData.lastRead
-                    this.book = ePub(bookData.url)   
-                    let meta = await __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__BookOps__["c" /* getBookMeta */])(bookData.url)
-                    document.title = meta.title || meta.bookTitle || "无题"
-                    if(cfi){
-                        this.book.locations.currentLocation = cfi
-                    }     
-                }
-                else {
-                    this.$router.push("/")
-                }
-            }
-        },
-        watch: {
-
+    },
+    data() {
+        return {
+            book: null,
+            font: null
         }
-    });
+    },
+    beforeDestroy(){
+        document.removeEventListener('keydown', this.keyEvent, false)
+        this.book = null
+        //  关闭监听事件。。。
+        this.$store._subscribers.pop()
+    },
+    async beforeMount(){
+        let fontFace = new FontFace("defaultText", "url(../static/fonts/SourceHanSerifSC-Regular.otf)", {})
+        this.font = await fontFace.load()
+    },
+    async mounted() {
+        var _this = this
+        
+        await this.loadBook()
+
+        //初始化渲染
+        let reader = this.$el.querySelector('.reader')
+        let rendition = this.book.renderTo(reader,{
+            width: "100%",
+            height: "100%",
+            // manager: "continuous",
+            flow: "paginated",//scorlled
+            spread: "none"
+        })       
+
+        rendition.hooks.content.register((content)=>{
+            content.document.fonts.add(_this.font)
+        })
+        
+        //初始化样式
+        this.setStyle(this.$store.state.reader.book)         
+        rendition.themes.default({
+            "*": {
+                'font-family': "defaultText"
+            }
+        });
+         
+        //开始渲染
+        rendition.start()
+        rendition.display( this.loadCFI() ) 
+
+        //初始化按键事件，在对应方法里处理
+        //准备把按键改为主进程监听
+
+        this.$el.addEventListener('wheel', this.keyEvent, false)
+        this.$el.addEventListener('keydown', this.keyEvent, false)
+        this.book.rendition.on("keydown", _this.keyEvent.bind(_this))
+
+        //初始化store监听
+        this.$store.subscribe((mutation,state)=>{
+            this.syncStore(mutation, state)
+        })
+
+        //。。。生成事件作为源
+        //加上消抖，手速太快的时候写入DB会有冲突，控制台一篇红色。。。
+
+        // var storeHandler = Rx.Observable.fromEvent(this.$el, 'storeChange')
+        // .distinctUntilChanged((pre,cur)=> _.isEqual(pre.detail.state.reader.book, cur.detail.state.reader.book))
+        // .throttleTime(100)           
+        // .subscribe((event)=>{
+        // })   
+        // var storeChange = new CustomEvent('storeChange',{detail: {mutation: mutation,state: state}})
+        // this.$el.dispatchEvent(storeChange)
+      
+
+
+        // 初始化阅读位置自动保存
+        this.book.rendition.on('locationChanged',( location )=>{
+            console.log("当前位置", location.start)
+            this.$store.commit("SET_LASTREAD",{cfi: location.start})
+        })
+
+    },
+    methods: {
+        syncStore(mutation, state){
+            if(this.$route.path == "/reader"){   
+                if(mutation.type == "SET_STYLE"){
+                    let book = state.reader.book
+                    this.setStyle(book)
+                    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__BookOps__["b" /* updateBook */])(book)
+                }
+                else if(mutation.type == "SET_LASTREAD"){
+                    let book = state.reader.book
+                    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__BookOps__["b" /* updateBook */])(book)
+                }
+            }
+        },
+        setStyle(book){
+            if(book.config && this.book){            
+                if(book.config["font-size"]){
+                    this.book.rendition.themes.fontSize(book.config["font-size"] + "px")
+                }
+                if(book.config["line-height"]){                    
+                    this.book.rendition.themes.override("line-height", book.config["line-height"] + "em")
+                }
+            }
+        },
+        keyEvent(event) {
+            if (event.key == "ArrowDown" || event.key == "ArrowRight" || event.deltaX > 0 || event.deltaY > 0) {
+                console.log("下")
+                this.pageNext()
+            }
+            else if (event.key == "ArrowUp" || event.key == "ArrowLeft" || event.deltaX < 0 || event.deltaY < 0) {
+                console.log("上")
+                this.pagePrev()
+            }
+        },
+        pageNext() {
+            this.book.rendition.next()
+
+        },
+        pagePrev() {
+            this.book.rendition.prev()
+        },
+        async loadBook() {
+            let bookData = this.$store.state.reader.book
+            if (bookData) {
+                this.book = ePub(bookData.url)   
+                let meta = await __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__BookOps__["c" /* getBookMeta */])(bookData.url)
+                document.title = meta.title || meta.bookTitle || "无题"
+            }
+            else {
+                this.$router.push("/")
+            }
+        },
+        loadCFI() {                
+            let bookData = this.$store.state.reader.book
+            let cfi = bookData.lastRead
+            return cfi ? cfi : undefined          
+        }
+    },
+    watch: {
+
+    }
+});
 
 
 /***/ }),
